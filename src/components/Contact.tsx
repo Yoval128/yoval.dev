ef03920e9ac784e8970b89e91876c1b7f0011a44
Yoval128 <yovalaviles812@gmail.com>
2026-05-02 19:00:21 -0600

    feat: servicio de mail

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,124 +1,143 @@
-import {motion} from "motion/react";
-import {fadeUp} from "@/lib/animations.ts";
+import { motion } from "motion/react";
+import { fadeUp } from "@/lib/animations.ts";
 import SectionHeader from "@/components/SectionHeader.tsx";
-import {Input} from "@/components/ui/input";
-import {Textarea} from "@/components/ui/textarea";
-import {Button} from "@/components/ui/button";
-import {useForm} from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
+import { useState } from "react";
 
-import {Field, FieldError} from "@/components/ui/field";
+import { Field, FieldError } from "@/components/ui/field";
 
 type contactFormValues = {
-    name: string,
-    company: string,
-    email: string,
-    phone: string,
-    message: string,
-}
+    name: string;
+    company: string;
+    email: string;
+    phone: string;
+    message: string;
+};
+
 const Contact = () => {
-        const form = useForm<contactFormValues>({
-            defaultValues: {
-                name: '',
-                company: '',
-                email: '',
-                phone: '',
-                message: '',
-            } // Se eliminó el punto y coma erróneo que tenías aquí
-        });
+    const [loading, setLoading] = useState(false);
 
-        const onSubmit = (values: contactFormValues) => {
-            console.log(values);
-        };
+    const form = useForm<contactFormValues>({
+        defaultValues: {
+            name: "",
+            company: "",
+            email: "",
+            phone: "",
+            message: "",
+        },
+    });
 
-        return (
-            <motion.section
-                initial='hidden'
-                whileInView='visible'
-                viewport={{once: true, amount: (0.8)}}
-                variants={fadeUp}
-                className='mt-10 scroll-mt-10 '
-                id='contact'>
+    const onSubmit = async (values: contactFormValues) => {
+        try {
+            setLoading(true);
 
-                <SectionHeader
-                    subtitle='Contacto'
-                    title='¿Tienes un proyecto o una oportunidad? Hablemos'
-                />
-                <form onSubmit={form.handleSubmit(onSubmit)}
-                      className="w-full mx-auto space-y-4 mt-10 bg-background p-2">
-                    <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-                        <Field>
-                            <Input
-                                placeholder='Nombre completo'
-                                className='bg-background border border-border text-foreground'
-                                {...form.register('name')}
-                                aria-invalid={!!form.formState.errors.name}/>
-                            {form.formState.errors.name && (
-                                <FieldError>
-                                    {form.formState.errors.name.message}
-                                </FieldError>
-                            )}
-                        </Field>
+            const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+            const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+            const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
-                        <Field>
-                            <Input
-                                placeholder='Nombre de la compañia'
-                                className='bg-background border border-border text-foreground'
-                                {...form.register('company')}
-                                aria-invalid={!!form.formState.errors.company}/>
-                            {form.formState.errors.company && (
-                                <FieldError>
-                                    {form.formState.errors.company.message}
-                                </FieldError>
-                            )}
-                        </Field>
+            await emailjs.send(
+                serviceId,
+                templateId,
+                {
+                    name: values.name,
+                    company: values.company,
+                    email: values.email,
+                    phone: values.phone,
+                    message: values.message,
+                },
+                publicKey
+            );
 
-                        <Field>
-                            <Input
-                                type='email'
-                                placeholder='Correo electronico'
-                                className='bg-background border border-border text-foreground'
-                                {...form.register('email')}
-                                aria-invalid={!!form.formState.errors.email}/>
-                            {form.formState.errors.email && (
-                                <FieldError>
-                                    {form.formState.errors.email.message}
-                                </FieldError>
-                            )}
-                        </Field>
+            console.log("Mensaje enviado correctamente");
 
+            form.reset();
 
-                        <Field>
-                            <Input
-                                type='tel'
-                                placeholder='Telefono'
-                                className='bg-background border border-border text-foreground'
-                                {...form.register('phone')}
-                                aria-invalid={!!form.formState.errors.phone}/>
-                            {form.formState.errors.phone && (
-                                <FieldError>
-                                    {form.formState.errors.phone.message}
-                                </FieldError>
-                            )}
-                        </Field>
-                    </div>
+        } catch (error) {
+            console.error("Error enviando mensaje:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <motion.section
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.8 }}
+            variants={fadeUp}
+            className="mt-10 scroll-mt-10"
+            id="contact"
+        >
+            <SectionHeader
+                subtitle="Contacto"
+                title="¿Tienes un proyecto o una oportunidad? Hablemos"
+            />
+
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="w-full mx-auto space-y-4 mt-10 bg-background p-2"
+            >
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+
                     <Field>
-                        <Textarea
-                            placeholder='Escribe tu mensaje'
-                            className="h-36 bg-background border border-border text-foreground"
-                            {...form.register('message')}
-                            aria-invalid={!!form.formState.errors.message}/>
-                        {form.formState.errors.message && (
+                        <Input
+                            placeholder="Nombre completo"
+                            {...form.register("name")}
+                        />
+                        {form.formState.errors.name && (
                             <FieldError>
-                                {form.formState.errors.message.message}
+                                {form.formState.errors.name.message}
                             </FieldError>
                         )}
                     </Field>
-                    <Button type='submit' className='bg-primary text-primary-foreground' size='lg'>
-                        Enviar mensaje
-                    </Button>
-                </form>
-            </motion.section>
-        )
-    }
-;
+
+                    <Field>
+                        <Input
+                            placeholder="Nombre de la compañía"
+                            {...form.register("company")}
+                        />
+                    </Field>
+
+                    <Field>
+                        <Input
+                            type="email"
+                            placeholder="Correo electrónico"
+                            {...form.register("email")}
+                        />
+                    </Field>
+
+                    <Field>
+                        <Input
+                            type="tel"
+                            placeholder="Teléfono"
+                            {...form.register("phone")}
+                        />
+                    </Field>
+
+                </div>
+
+                <Field>
+                    <Textarea
+                        placeholder="Escribe tu mensaje"
+                        className="h-36"
+                        {...form.register("message")}
+                    />
+                </Field>
+
+                <Button
+                    type="submit"
+                    size="lg"
+                    disabled={loading}
+                >
+                    {loading ? "Enviando..." : "Enviar mensaje"}
+                </Button>
+            </form>
+        </motion.section>
+    );
+};
+
 export default Contact;
